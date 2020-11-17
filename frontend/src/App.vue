0a78055252eb34314cd1152a7fb02102ff055396
AidenNeale <an12@sanger.ac.uk>
2020-11-17 17:53:10 +0000

    DropDown Tests

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,42 +2,55 @@
   <div id="app">
     <div id='dropdownlist' style="margin:50px auto 0; width:250px;">
       <br>
-      <ejs-dropdownlist :dataSource='localData' :fields='localField'
-      placeholder='Select A Flavour' popupWidth="250px" popupHeight="200px">
+      <ejs-dropdownlist :dataSource='remoteData' :fields='remoteFields'
+      placeholder='Select A Flavour' popupWidth="250px" popupHeight="200px"
+      allowFiltering='true'>
       </ejs-dropdownlist>
     </div>
   </div>
 </template>
 
 <script>
-import Vue from 'vue';
-import { DropDownListPlugin } from '@syncfusion/ej2-vue-dropdowns';
-Vue.use(DropDownListPlugin);
+  import Vue from 'vue';
+  import { DropDownListPlugin } from '@syncfusion/ej2-vue-dropdowns';
+  import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
+  Vue.use(DropDownListPlugin);
+  var remoteDataSource = new DataManager({
+    url: 'http://172.27.17.127:5000/hail/api/flavors/',
+    adaptor: new WebApiAdaptor,
+    crossDomain: true
+  });
+  export default Vue.extend({
+    data: function() {
+      return {
 
-export default Vue.extend({
-  data: function() {
-    return {
-      localData:[
-        { Id: 'foo1', Flavor: 'bar1' }, 
-        { Id: 'foo2', Flavor: 'bar2' },
-        { Id: 'foo3', Flavor: 'bar3' }
-      ],
-      localField: {value: 'Id', text: 'Flavor'}
-    };
-  },
-  mounted(){
-    this.getFlavors()
-  },
+        remoteData: remoteDataSource,
+        remoteFields: { value: 'Id', text: 'Name', groupBy: 'Category' }
 
-  methods: {
-    async getFlavors(){
-        const response = await fetch('172.27.17.127:5000/hail' + '/backend')
-        console.log(response)
-        const wholeResponse = await response.json()
-        print(wholeResponse)
-    }
-  },
-})
+
+/*
+        localData:[
+          { Id: 'foo1', Flavor: 'bar1' }, 
+          { Id: 'foo2', Flavor: 'bar2' },
+          { Id: 'foo3', Flavor: 'bar3' }
+        ],
+        localField: {value: 'Id', text: 'Flavor'}
+*/
+      };
+    },
+
+
+    mounted(){
+      this.getFlavors()
+    },
+
+    methods: {
+      async getFlavors(){
+          const response = await fetch('http://172.27.17.127:5000/hail' + '/api/flavors')
+          console.log(response)
+      }
+    },
+  })
 </script>
 
 <style>
