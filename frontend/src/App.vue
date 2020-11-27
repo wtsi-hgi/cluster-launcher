d54b8a4e5b9251aeba6c381586482cc077b9392a
AidenNeale <an12@sanger.ac.uk>
2020-11-27 12:31:40 +0000

    Pushed frontend changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,54 +1,53 @@
 <template>
   <div id="app">
-    <input type="publicKey" v-model="pkey" placeholder="Public-Key">
+    <input type="publicKey" v-model="pkey" placeholder="Public Key">
     <input type="numOfWorkers" v-model="workers" placeholder="Number of Workers">
     <input type="pass" v-model="password" placeholder="Password">
     <input type="flavour" v-model="flavour" placeholder="Flavour">
-    <button rounded> Rounded Button </button>
+    <v-button :pubkey=this.pkey :workers=this.workers :password=this.password :flavor=this.flavour> Launch Cluster </v-button>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import Vuetify from 'vuetify';
-
-Vue.use(Vuetify);
-
-export default {
-  name: 'App',
-  components: {},
-  data: () => ({
-    pkey:     'abc',
-    workers:  '',
-    password: '',
-    flavour:  ''
-  })
-}
+  import Button from './components/Button.vue'
+  export default {
+    name: 'App',
+    components: {
+      'v-button': Button      
+    },
+    data: () => ({
+      pkey:     '',
+      workers:  '',
+      password: '',
+      flavour:  ''
+    })
+  }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-input[type=flavour] {
-  width: 45%;
-  margin: 10px;
-}
-input[type=publicKey] {
-  width:45%;
-  margin: 10px;
-}
-input[type=numOfWorkers] {
-  width:45%;
-  margin: 10px;
-}
-input[type=pass] {
-  width:45%;
-  margin: 10px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  input[type=flavour] {
+    width: 45%;
+    margin: 10px;
+  }
+  input[type=publicKey] {
+    width: 45%;
+    margin:10px;
+  }
+  input[type=numOfWorkers] {
+    width: 45%;
+    margin: 10px;
+  }
+  input[type=pass] {
+    width: 45%;
+    margin: 10px;
+  }
+
 </style>
---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,38 @@
+<template>
+<button @click="onClick" class="Button">
+  <slot>Button</slot>
+</button>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    props: {
+      pubkey: {required: true, type:String},
+      workers: {required: true, type:String},
+      password: {required: true, type:String},
+      flavor: {required: true, type:String}      
+    },
+    methods: {
+      onClick: function() {
+        if (this.pubkey != '' && this.workers != '' && this.password != '' && this.flavor != '') {
+          const requestOptions = { title: "VUE TEST" };
+          axios.get("http://172.27.17.127:5000/hail/frontend", requestOptions)
+            .then(response => this.requestOptionsID = response.data.id);
+          console.log(this.workers)
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+.Button{
+  font-size:24px;
+  color: white;
+  margin: 4px 2px;
+  text-align: center;
+  display: inline-block;
+  background-color: #4CAF50;
+}
+</style>
