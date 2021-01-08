4f4ef012860d25da4ab223ddd8de42ee5feb6fcb
AidenNeale <an12@sanger.ac.uk>
2021-01-08 13:44:30 +0000

    changed frontend app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,40 +1,56 @@
 <template>
   <div id="app">
-    <div class="main" v-show='this.status'>
-      <input type="publicKey" v-model="pkey" placeholder="Public Key">
-      <input type="numOfWorkers" v-model="workers" placeholder="Number of Workers">
-      <input type="pass" v-model="password" placeholder="Password">
-      <input type="flavour" v-model="flavour" placeholder="Flavour">
-      <v-button :status=this.status :pubkey=this.pkey :workers=this.workers :password=this.password :flavor=this.flavour @update-status="update"> Launch Cluster </v-button>
-    </div>
-    
-    <div v-show='!this.status'>
+    <div class="main" v-show='!this.status'>
       <div class="header">
         <div class="header-text">
           <p>Date/Time: </p>
         </div>
+      </div>
+      <input type="publicKey" v-model="pkey" placeholder="Public Key">
+      <input type="numOfWorkers" v-model="workers" placeholder="Number of Workers">
+      <input type="pass" v-model="password" placeholder="Password">
+      <input type="flavour" v-model="flavour" placeholder="Flavour">
+      <v-button :status=this.status :pubkey=this.pkey :workers=this.workers :password=this.password :flavor=this.flavour
+         @update-status="update"> Launch Cluster </v-button>
+    </div>
+    
+    <div v-show='this.status'>
+      <div class="header">
+        <div class="header-text">
+          <p>Date/Time: </p>
+        </div>
+        <d-button :status=this.status :pubkey=this.pkey :workers=this.workers :password=this.password :flavor=this.flavour
+          @update-status="update"> Destroy Cluster </d-button>      
+      </div>
+      <div> 
+        <iframe src="http://172.27.23.16/jupyter/" loading="lazy"></iframe>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import Button from './components/Button.vue'
+  import Button from './components/CreateButton.vue'
+  import DButton from './components/DestroyButton.vue'
   export default {
     name: 'App',
     components: {
-      'v-button': Button      
+      'v-button': Button,
+      'd-button': DButton
     },
     data: () => ({
       pkey:     '',
       workers:  '',
       password: '',
       flavour:  '',
-      status: true
+      url: 'www.google.com',
+      status: false
     }),
     methods:{
       update(newStatus){
         this.status=newStatus;
+        console.log(newStatus)
+        console.log(this.status)
       }
     }
   }
@@ -47,7 +63,6 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
   }
   input[type=flavour] {
     width: 45%;
@@ -65,18 +80,19 @@
     width: 45%;
     margin: 10px;
   }
-  /* Header/Logo Title */
+  /* Header */
   .header {
-    margin-top: 0px;
-    padding: 10px;
     background: #73FF75;
-    color: white;
-    font-size: 12px;
-    opacity:50%;
+    width: 100%;
+    opacity: 50%;
+    margin-bottom: 40px;
+    float: left;
   }
   .header-text {
     color: #052A00;
-    position: left;
-    left: 10px;
+    font-size: 16px;
+    float: left;
+    margin: 0px 5px;
   }
+
 </style>
