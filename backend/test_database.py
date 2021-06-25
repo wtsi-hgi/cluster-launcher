c04b9df249ad870fa3b9d03e0264b59902ff7ef6
Aiden Neale <an12@sanger.ac.uk>
2021-06-25 12:21:50 +0100

    Pushing Basic Tests

---
+++ backend/test_database.py
@@ -0,0 +1,38 @@
+import tempfile
+import os
+import sqlite3
+import subprocess
+from unittest import mock
+
+from unittest.mock import Mock, patch
+from unittest import TestCase
+
+import database
+
+
+class TestDatabase(TestCase):
+  '''
+  def test_database_file(self):
+    #db, cursor = database.initialise_database()
+    file_path = "/home/ubuntu/backend/clusters/cluster-networking.db"
+    isFile = os.path.isfile(file_path)
+    self.assertEqual(isFile, True)
+  '''
+
+  @patch(database.DATABASE_NAME)
+  def test_users(self, mocked_db):
+
+    with tempfile.NamedTemporaryFile() as tempFile:
+      mocked_db = tempFile.name
+      database.add_user('an12')
+      result = check_database_entry(mocked_db, "username", "users")
+      print(result)
+      self.assertEqual()
+
+
+def check_database_entry(temp_file, option, table):
+  db = sqlite3.connect(temp_file)
+  cursor = db.cursor()
+  query = "SELECT" + option + "FROM" + table
+  cursor.execute(query)
+  return(cursor.fetchone())
---
+++ backend/test_network.py
@@ -0,0 +1,40 @@
+import os
+from unittest import TestCase
+from unittest.mock import Mock, patch
+import network
+import neutronclient.v2_0
+
+class TestNetworking(TestCase):
+    @patch.dict(os.environ, {"OS_AUTH_URL": "auth/path", "OS_USERNAME":"test-user","OS_PASSWORD":"test-password"})
+    def test_credentials(self):
+        creds = network._neutron('test-tenant')
+        self.assertIsInstance(creds, neutronclient.v2_0.client.Client)
+
+
+'''
+    #This is currently not testing anything
+    #This needs to be turned into integration testing (actually testing the network comes up)
+    @patch('network.database')
+    def test_create(self, mocked_db):
+        connection = Mock()
+        #Create Dummy Networks
+        network1, network2 = DummyNetwork("test-network1", "1"), DummyNetwork("test-network2", "2")
+        #Mock a list of networks
+        connection.network.networks.return_value = [network1, network2]
+        mocked_db.initialise_database.return_value = mocked_db, "Cursor"
+        
+        
+        network.create(connection, 'test-user', 'test-tenant')
+
+
+class DummyNetwork():
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+class DummyDatabase():
+    def __init__(self):
+        pass
+    def close(self):
+        pass
+'''
